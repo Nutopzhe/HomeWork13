1741f33c5b4872335516c2e57dcce4ea231d020f
Nikolay Rybakov <mr.Nikolay.Rybakov@yandex.ru>
2021-08-26 23:52:25 +0300

    Added method reader. Created and filled  2 files (fio, questions)

--- src/main/java/UtilClass.java
+++ src/main/java/UtilClass.java
@@ -1,5 +1,28 @@
-public class UtilClass {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class UtilClass {
+    static final File FILE_FIO = new File("src/main/resources/fio.txt");
+    static final File FILE_QUESTIONS = new File("src/main/resources/questions.txt");
+
+    public static void main(String[] args) {
+        List<String> listFio = new ArrayList<>();
+        List<String> listQuestions = new ArrayList<>();
+
+        methodReader(listFio, FILE_FIO);
+        methodReader(listQuestions, FILE_QUESTIONS);
+
+    }
+
+    public static void methodReader(List<String> list, File filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
